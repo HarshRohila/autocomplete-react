87a3d4518fe3b6d20d4de107ccc80e6b2e90142e
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-08-25 07:47:59 +0530

    using https for apis

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -5,12 +5,12 @@ const apikey = environment[process.env.NODE_ENV].API_KEY
 
 const moviesService = {
   searchByTitle(title: string): Promise<Movie[]> {
-    return fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apikey}&type=movie`)
+    return fetch(`https://www.omdbapi.com/?s=${title}&apikey=${apikey}&type=movie`)
       .then((res) => res.json())
       .then((res) => res.Search)
   },
   getById(id: string): Promise<MovieDetails> {
-    return fetch(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}`).then((res) => res.json())
+    return fetch(`https://www.omdbapi.com/?i=${id}&apikey=${apikey}`).then((res) => res.json())
   },
 }
 
